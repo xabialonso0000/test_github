52baf81845c0542df886558733143ebe00a491af
Koichi Suzuki <xabi_alonso0000@yhaoo.co.jp>
2026-07-09 17:39:57 +0900

    ciのログ出力

--- front/vitest.config.ts
+++ front/vitest.config.ts
@@ -9,6 +9,11 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+
+      reporters:[
+        'default',
+        ['junit', { outputFile: 'test-results/junit.xml'}]
+      ],
     },
   }),
 )
