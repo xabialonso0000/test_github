e72fe9f12e87c39832d0f166538d06801977668b
xabi_ <xabi_@DESKTOP-5VNSFE8.localdomain>
2026-07-09 15:05:00 +0900

    second

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,19 +1,19 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      Youve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+</script>
+
+
 
 <style scoped>
 h1 {
--- front/vitest.config.ts
+++ front/vitest.config.ts
@@ -1,7 +1,6 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
-import vue from '@vitejs/plugin-vue'
 
 export default mergeConfig(
   viteConfig,
@@ -11,12 +10,5 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
-  plugins: [
-    vue()
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
   }),
 )
